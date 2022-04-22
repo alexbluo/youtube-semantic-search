050b2e457280385935440f01fd3186093bd80126
Alex Luo <alexluo92823@gmail.com>
2022-04-22 12:05:37 -0400

    add z score normalization using standard deviation and push to every object in the transcript array

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -15,7 +15,7 @@ const Watch: NextPage = (props) => {
         url={`https://youtube.com/watch?v=${router.query.v}`}
         key={process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}
       />
-      {/* {props.transcript} */}
+      {props.scores}
     </div>
   );
 };
@@ -24,7 +24,7 @@ type Transcript = Array<{
   text: string;
   start: number;
   duration: number;
-  score?: number;
+  zscore?: number;
 }>;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -33,12 +33,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   });
   const openai = new OpenAIApi(configuration);
 
+  const scores: Transcript = [];
+
   const pyscript = spawn("python", [
     "./scripts/transcript.py",
-    context.query.v,
+    context.query.v as string,
   ]);
 
-  const transcriptWithScores: Transcript = [];
   pyscript.stdout.on("data", async (data: Buffer) => {
     const transcript: Transcript = eval(data.toString());
 
@@ -47,21 +48,44 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       const slice = transcript.slice(0, 200);
       const documents = slice.map(({ text }) => text);
 
-      const response = await openai.createSearch("babbage", {
+      const {
+        // shorthand for response.data.data
+        data: { data },
+      } = await openai.createSearch("babbage", {
         documents: documents,
         query: "language",
       });
 
-      console.log(response.data.data)
-      // transcriptWithScores.push(...response.data.data);
+      const zscores = zscore(data!.map(({ score }) => score!));
+      slice.forEach((obj, i) => obj.zscore = zscores[i])
+      scores.push(...slice)
     }
+
+    console.log(scores);
   });
 
   return {
     props: {
-      // transcript,
+      scores,
     },
   };
 };
+
+/**
+ * calculate the z-score of each score by normaling with mean and standard deviation
+ */
+function zscore(scores: Array<number>) {
+  const size = scores.length;
+  const total = scores.reduce((acc, score) => acc + score, 0);
+  const mean = total / size;
+
+  const standardDeviation = Math.sqrt(
+    scores.reduce((acc, score) => acc + Math.pow(score - mean, 2), 0) / size
+  );
+
+  const zscores = scores.map((score) => (score - mean) / standardDeviation);
+
+  return zscores;
+}
 
 export default Watch;
